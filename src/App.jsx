8035a3a4a0ae12dfe09c3d47eef6ab48e6e72a69
amritansh-pandey <amritansh.p1010@gmail.com>
2022-07-15 20:31:44 +0530

    initial layout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Home from './pages/Home/Home';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hi</h1>
-    </div>
-  )
-}
+const App = () => <Home />
 
 export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+      <div>
+        {/* Search Bar */}
+
+        {/* Side Panels */}
+
+        {/* List & EMpty Views */}
+      </div>
+    )
+  }
+
+export default Home;
